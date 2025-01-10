d78434fd9174d16e86814a07ea149bd0f9065454
Adam Maas <adamjm1220@gmail.com>
2025-01-10 14:16:27 -0500

    Initial Setup

---
+++ src/main/java/am/library/HttpM/HttpMApplication.java
@@ -0,0 +1,13 @@
+package am.library.HttpM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/am/library/HttpM/HttpMApplicationTests.java
@@ -0,0 +1,13 @@
+package am.library.HttpM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
