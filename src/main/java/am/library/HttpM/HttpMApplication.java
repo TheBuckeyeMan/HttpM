8407076a5b1b0944b848f2839f07965a69401878
Adam Maas <adamjm1220@gmail.com>
2025-01-13 18:04:06 -0500

    Updated to cd.yml

--- src/main/java/am/library/HttpM/HttpMApplication.java
+++ src/main/java/am/library/HttpM/HttpMApplication.java
@@ -3,11 +3,11 @@ package am.library.HttpM;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HttpMApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HttpMApplication.class, args);
+		
 	}
-
 }
